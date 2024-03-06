ab83563be724f96728503c03089ae66ece8757c1
regazo <z.mbame11@gmail.com>
2024-03-06 23:53:30 +0000

    commiting before implementation

--- WorksheetTwo__3074880/src/test/java/griffith/Conversions.java
+++ WorksheetTwo__3074880/src/test/java/griffith/Conversions.java
@@ -1,5 +1,40 @@
 package griffith;
-
+//zakarea erezzaghi
 public class Conversions {
 
+    // Method stub that coverts the Celsius to Fahrenheit
+    public double celsiusToFahrenheit(double celsius) {
+        // we implement later
+        return 0.0;
+    }
+
+    // Method stub that coverts the Fahrenheit to Celsius
+    public double fahrenheitToCelsius(double fahrenheit) {
+        // we implement later
+        return 0.0;
+    }
+
+    // Method stub that coverts the miles to kilometers
+    public double milesToKilometer(double miles) {
+        // we implement later
+        return 0.0;
+    }
+
+    // Method stub that coverts the kilometers to miles
+    public double kilometerToMiles(double kilometers) {
+        // we implement later
+        return 0.0;
+    }
+
+    // Method stub that coverts the uppercase letters to the lowercase letters
+    public String upperCaseToLowerCase(String input) {
+        // we implement later
+        return null;
+    }
+
+    // Method stub that coverts the lowercase letters to the uppercase letters
+    public String lowerCaseToUpperCase(String input) {
+        // we implement later
+        return null;
+    }
 }
