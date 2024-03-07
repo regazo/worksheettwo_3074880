0e5a4edaa78dd011d7a97bad953251197cdfbadf
regazo <z.mbame11@gmail.com>
2024-03-07 01:52:03 +0000

    new pom.xml and i accidentally commited previously all of conversions code so this is the commit fix to do each method in test class with failure then without failure and to be fixed

--- WorksheetTwo__3074880/src/test/java/griffith/ConversionsTest.java
+++ WorksheetTwo__3074880/src/test/java/griffith/ConversionsTest.java
@@ -1,23 +1,17 @@
 package griffith;
 //zakarea erezzaghi 3074880
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class ConversionsTest {
 
     // Unit test for celsiusToFahrenheit method
     @Test
     public void testCelsiusToFahrenheit() {
-        double result1 = Conversions.celsiusToFahrenheit(0);
-        assertEquals(32, result1, "0 Celsius should be 32 Fahrenheit");
-
-        double result2 = Conversions.celsiusToFahrenheit(100);
-        assertEquals(212, result2, "100 Celsius should be 212 Fahrenheit");
-
-        double result3 = Conversions.celsiusToFahrenheit(-40);
-        assertEquals(-40, result3, "-40 Celsius should be -40 Fahrenheit");
+        assertEquals("0 Celsius should be 32 Fahrenheit", 32.0, Conversions.celsiusToFahrenheit(0), 0.001);
+        assertEquals("100 Celsius should be 212 Fahrenheit", 212.0, Conversions.celsiusToFahrenheit(100), 0.001);
+        assertEquals("-40 Celsius should be -40 Fahrenheit", -40.0, Conversions.celsiusToFahrenheit(-40), 0.001);
     }
 }
-
- 
+    
