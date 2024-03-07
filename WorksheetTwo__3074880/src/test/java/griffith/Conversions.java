23f518102017424cd8d66f6198fed4f8c72e1068
regazo <z.mbame11@gmail.com>
2024-03-07 00:33:13 +0000

    commiting after first method added to test class

--- WorksheetTwo__3074880/src/test/java/griffith/Conversions.java
+++ WorksheetTwo__3074880/src/test/java/griffith/Conversions.java
@@ -1,40 +1,42 @@
 package griffith;
-//zakarea erezzaghi
+//zakarea erezzaghi 3074880
 public class Conversions {
 
-    // Method stub that coverts the Celsius to Fahrenheit
-    public double celsiusToFahrenheit(double celsius) {
-        // we implement later
-        return 0.0;
-    }
+	    // Method that converts Celsius to Fahrenheit
+	    public double celsiusToFahrenheit(double celsius) {
+	        // Implement conversion logic from Celsius to Fahrenheit
+	        return (celsius * 9 / 5) + 32;
+	    }
 
-    // Method stub that coverts the Fahrenheit to Celsius
-    public double fahrenheitToCelsius(double fahrenheit) {
-        // we implement later
-        return 0.0;
-    }
+	    // Method that converts Fahrenheit to Celsius
+	    public double fahrenheitToCelsius(double fahrenheit) {
+	        // Implement conversion logic from Fahrenheit to Celsius
+	        return (fahrenheit - 32) * 5 / 9;
+	    }
 
-    // Method stub that coverts the miles to kilometers
-    public double milesToKilometer(double miles) {
-        // we implement later
-        return 0.0;
-    }
+	    // Method that converts miles to kilometers
+	    public double milesToKilometer(double miles) {
+	        // Implement conversion logic from miles to kilometers
+	        return miles * 1.60934;
+	    }
 
-    // Method stub that coverts the kilometers to miles
-    public double kilometerToMiles(double kilometers) {
-        // we implement later
-        return 0.0;
-    }
+	    // Method that converts kilometers to miles
+	    public double kilometerToMiles(double kilometers) {
+	        // Implement conversion logic from kilometers to miles
+	        return kilometers * 0.62137;
+	    }
 
-    // Method stub that coverts the uppercase letters to the lowercase letters
-    public String upperCaseToLowerCase(String input) {
-        // we implement later
-        return null;
-    }
+	    // Method that converts uppercase letters to lowercase letters
+	    public String upperCaseToLowerCase(String input) {
+	        // Implement conversion logic to convert uppercase letters to lowercase letters
+	        return input.toLowerCase();
+	    }
 
-    // Method stub that coverts the lowercase letters to the uppercase letters
-    public String lowerCaseToUpperCase(String input) {
-        // we implement later
-        return null;
-    }
-}
+	    // Method that converts lowercase letters to uppercase letters
+	    public String lowerCaseToUpperCase(String input) {
+	        // Implement conversion logic to convert lowercase letters to uppercase letters
+	        return input.toUpperCase();
+	    }
+	}
+
+	
--- WorksheetTwo__3074880/src/test/java/griffith/ConversionsTest.java
+++ WorksheetTwo__3074880/src/test/java/griffith/ConversionsTest.java
@@ -1,5 +1,23 @@
 package griffith;
-//zakarea erezzaghi
+//zakarea erezzaghi 3074880
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ConversionsTest {
+
+    // Unit test for celsiusToFahrenheit method
+    @Test
+    public void testCelsiusToFahrenheit() {
+        double result1 = Conversions.celsiusToFahrenheit(0);
+        assertEquals(32, result1, "0 Celsius should be 32 Fahrenheit");
+
+        double result2 = Conversions.celsiusToFahrenheit(100);
+        assertEquals(212, result2, "100 Celsius should be 212 Fahrenheit");
+
+        double result3 = Conversions.celsiusToFahrenheit(-40);
+        assertEquals(-40, result3, "-40 Celsius should be -40 Fahrenheit");
+    }
+}
 
  
