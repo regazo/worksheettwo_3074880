c25d10bc41aed774721c2017d236d116d7980018
regazo <z.mbame11@gmail.com>
2024-03-07 01:55:11 +0000

    next method without implementation

--- WorksheetTwo__3074880/src/test/java/griffith/ConversionsTest.java
+++ WorksheetTwo__3074880/src/test/java/griffith/ConversionsTest.java
@@ -13,5 +13,12 @@ public class ConversionsTest {
         assertEquals("100 Celsius should be 212 Fahrenheit", 212.0, Conversions.celsiusToFahrenheit(100), 0.001);
         assertEquals("-40 Celsius should be -40 Fahrenheit", -40.0, Conversions.celsiusToFahrenheit(-40), 0.001);
     }
+
+//Unit test for fahrenheitToCelsius method
+@Test
+public void testFahrenheitToCelsius() {
+    assertEquals("32 Fahrenheit should be 0 Celsius", 0.0, Conversions.fahrenheitToCelsius(32), 0.001);
+    assertEquals("212 Fahrenheit should be 100 Celsius", 100.0, Conversions.fahrenheitToCelsius(212), 0.001);
+    assertEquals("-40 Fahrenheit should be -40 Celsius", -40.0, Conversions.fahrenheitToCelsius(-40), 0.001);
 }
     
